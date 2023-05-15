ab968f1aab50b6f1dd74bcb834a0de214b7da6a2
Sonia <124130978+Sonia-Uwamahoro@users.noreply.github.com>
2023-05-15 16:10:08 +0300

    Update search.py

--- search.py
+++ search.py
@@ -14,3 +14,27 @@ my_list = ["banana", "avocado", "carrots", "peas"]
 prduct = "avocado"
 index = search_func(my_list, prduct)
 print(index) 
+
+# second trial
+
+
+class DictionarySearch:
+    def __init__(self, products):
+        self.products = products
+
+    def search_product(self, key, value):
+        for product, amount in self.products.items():
+            if product == key and amount == value:
+                return (product, amount)
+        return None
+    
+
+my_products = {'banana': 5, 'avocados': 6, 'peas': 9}
+search = DictionarySearch(my_products)
+
+result = search.search_product('peas', 9)
+if result:
+    print(f"({result[0]}, {result[1]}) is available")
+else:
+    print("product not found.")
+
